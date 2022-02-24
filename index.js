83156c7eebfd5d9dacb1391d7c67f10a092c890c
Badi-Haki Martin <jojackblack@gmail.com>
2022-02-24 10:55:26 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+
+let num1=31;
+let num2=2;
+const multiply=num1*num2;
+const random=Math.round(Math.random()*1989)*1;
+let num3=20;
+let num4=8;
+const mod=num3%num4;
+const max=Math.max(num1/2, num2, num3, num4);
+console.log(max);
